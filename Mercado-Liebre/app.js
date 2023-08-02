3a97105892a899748a6553ef6ad8309b1b7a1f8d
Ludmila Mansilla <90283512+ludmilamansilla@users.noreply.github.com>
2023-08-02 00:58:43 -0300

    Add files via upload

---
+++ Mercado-Liebre/app.js
@@ -0,0 +1,27 @@
+const express = require("express")
+let app = express()
+const PORT = 3000
+let path = require("path")
+
+
+app.use(express.static("public"))
+
+
+// RUTAS
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/home.html"))
+})
+
+app.get("/register",(req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+})
+
+app.get("/login",(req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+})
+
+
+app.listen(PORT, () => console.log(`
+Servidor escuchando en el puerto ${PORT}
+http://localhost:${PORT}
+`))
